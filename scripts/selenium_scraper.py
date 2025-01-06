3dcbd932afe534b279c1cd95ac435c9a00e311b2
christoptimist <christoptimist>
2025-01-06 15:27:30 +0800

    Update user_agent to more flexible

--- scripts/selenium_scraper.py
+++ scripts/selenium_scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium_stealth import stealth
+from fake_useragent import UserAgent
 import random
 
 # Configuration setup for service chrome.
@@ -40,17 +41,8 @@ driver = webdriver.Chrome(options=option,service=service)
 # Change the property value of the navigator for webdriver to undefined
 driver.execute_script("Object.defineProperty(navigator,'webdriver',{get:() => undefined})")
 
-user_agents = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
-]
-
-user_agent = random.choice(user_agents)
+ua_tmp = UserAgent()
+user_agent = ua_tmp.random
 
 # Add the random pick from the list of user_agents into browser properties.
 option.add_argument(f'user-agent={user_agent}')
