cf2317b60905ce61e0411b22e4742c250f54896a
christoptimist <christoptimist>
2025-01-10 22:04:45 +0800

    added_main_file

---
+++ scripts/web_scrape/main.py
@@ -0,0 +1,18 @@
+# Usage
+from web_scrape.classes.chrome_options import ChromeOptions
+from web_scrape.classes.chrome_web_driver_factory import ChromeWebDriverFactory
+from web_scrape.classes.stealth_configuration import StealthConfiguration
+from web_scrape.classes.web_driver_manager import WebDriverManager
+
+chrome_options = ChromeOptions()
+chrome_options.set_window_size(1200, 800)
+# ... other options
+
+stealth_config = StealthConfiguration() 
+
+driver_factory = ChromeWebDriverFactory(chrome_options, stealth_config)
+driver_manager = WebDriverManager(driver_factory)
+driver = driver_manager.get_driver() 
+
+driver.get('https://google.com/recaptcha/api2/demo')
+
