3ca86dcd3416889a0bbaa164aef3dfd5eea4ae64
christoptimist <christoptimist>
2025-01-10 23:07:38 +0800

    add_user_agent.py

--- scripts/web_scrape/main.py
+++ scripts/web_scrape/main.py
@@ -3,10 +3,13 @@ from web_scrape.classes.chrome_options import ChromeOptions
 from web_scrape.classes.chrome_web_driver_factory import ChromeWebDriverFactory
 from web_scrape.classes.stealth_configuration import StealthConfiguration
 from web_scrape.classes.web_driver_manager import WebDriverManager
+from web_scrape.classes.user_agents import UserAgents
+
+user_agent = UserAgents()
 
 chrome_options = ChromeOptions()
 chrome_options.set_window_size(1200, 800)
-# ... other options
+chrome_options.set_user_agent(f'--user-agent={user_agent.get_random_user_agent()}')
 
 stealth_config = StealthConfiguration() 
 
@@ -15,4 +18,3 @@ driver_manager = WebDriverManager(driver_factory)
 driver = driver_manager.get_driver() 
 
 driver.get('https://google.com/recaptcha/api2/demo')
-
