3ebdd4fd2353ba3310c6e564755e4a8522be32db
christoptimist <christoptimist>
2025-01-16 13:10:40 +0800

    remove_unnecessary_files

--- scripts/automated_ticket_booking/captchas/interfaces/i_captcha_solver.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ICaptchaSolver(ABC):
-    @abstractmethod
-    def solve(self):
-        pass
--- scripts/automated_ticket_booking/web_browser/classes/service_configuration.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-class ServiceConfiguration:
-    def __init__(self):
-        self.service = Service(executable_path=ChromeDriverManager().install())
-
-    def get_service_configuration(self):
-        return self.service
