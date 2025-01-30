c6b46e0b5336fc8afdb3f4c17ed7c74f6821474c
christoptimist <christoptimist@gmail.com>
2025-01-30 14:59:57 +0800

    add_gitignore

--- scripts/sauce-demo-automation/utilities/config_reader.py
+++ scripts/sauce-demo-automation/utilities/config_reader.py
@@ -1,4 +1,6 @@
+import os
 import yaml
+import logging
 
 def _load_config(file_path="config/dev.yaml"):
     """
@@ -6,5 +8,12 @@ def _load_config(file_path="config/dev.yaml"):
     :param file_path: Path to the YAML file
     :return: Dictionary
     """
-    with open(file_path, 'r') as file:
-        return yaml.safe_load(file)
+    try:
+        with open(file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except FileNotFoundError:
+        logging.info(f"File not found: {file_path}")
+        raise
+    except yaml.YAMLError as e:
+        logging.error(f"Error loading YAML file: {e}")
+        raise
